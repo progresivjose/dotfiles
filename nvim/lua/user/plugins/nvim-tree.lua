2a2e4a97e11a8bba6e3c1c851f0ad1607b0d240d
José Manuel González Ricart <jmanuelgricart@gmail.com>
2023-06-07 13:10:30 -0400

    initial commit

---
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -0,0 +1,20 @@
+require('nvim-tree').setup({
+  git = {
+    ignore = false,
+  },
+  renderer = {
+    highlight_opened_files = '1',
+    group_empty = true,
+    icons = {
+      show = {
+        folder_arrow = false,
+      },
+    },
+    indent_markers = {
+      enable = true,
+      -- inline_arrows = false,
+    },
+  },
+})
+
+vim.keymap.set('n', '<leader>n', ':NvimTreeFindFileToggle<CR>')
